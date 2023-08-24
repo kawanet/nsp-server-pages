7683c628e7b08f3ddd4c53c91796bf133344e190
Yusuke Kawasaki <u-suke@kawa.net>
2023-08-24 15:35:04 +0900

    bundle() uses concat()

--- src/bundle.ts
+++ src/bundle.ts
@@ -1,56 +1,35 @@
 import type {NSP} from "../index.js"
-
-const isPromise = (v: any): v is Promise<any> => v && (typeof v.then === "function");
-
-const join = (a: string, b: string): string => (a == null ? b : (b == null ? a : a + b));
+import {concat} from "./concat.js";
 
 export const bundle = <T>(array: ArrayLike<NSP.Node<T>>, start?: number, end?: number): NSP.NodeFn<T> => {
     start = +start || 0;
     end = +end || array?.length || 0;
 
+    // empty
     if (end <= start) {
         return () => null;
     }
 
+    // single item
     if (end - 1 === start) {
         const node = array[start];
         return (typeof node === "function") ? node : () => node;
     }
 
+    // multiple items
     return context => {
-        let result: string;
-        let promise: Promise<string>;
+        let results: (string | Promise<string>)[] = [];
 
         for (let i = start; i < end; i++) {
-            let v: string | Promise<string> | NSP.NodeFn<T> = array[i];
+            const item: string | NSP.NodeFn<T> = array[i];
 
-            if (promise) {
-                promise = promise.then(result => {
-                    if (typeof v === "function") {
-                        v = v(context);
-                    }
-
-                    if (isPromise(v)) {
-                        return v.then(v => join(result, v));
-                    } else {
-                        return join(result, v);
-                    }
-                });
+            if (typeof item === "function") {
+                results.push(item(context));
             } else {
-                if (typeof v === "function") {
-                    v = v(context);
-                }
-
-                if (isPromise(v)) {
-                    // upgrade to async mode
-                    promise = v.then(v => join(result, v));
-                } else {
-                    // sync mode per default
-                    result = join(result, v);
-                }
+                results.push(item);
             }
         }
 
-        return promise || result;
+        return concat(results);
     };
 };
--- test/110.bundle.ts
+++ test/110.bundle.ts
@@ -48,7 +48,7 @@ describe(TITLE, () => {
     });
 
     it("multiple async functions", async () => {
-        const fn = await nsp.bundle(
+        const fn = nsp.bundle(
             async () => "Foo",
             async () => "Bar",
             async () => "Buz",
